2e6d5362294b453ac24d3660cfc426ba714756e1
Christopher Lemelin <clemelin2@gmail.com>
2018-03-31 11:23:30 -0400

    added dropdown

--- static/index.js
+++ static/index.js
@@ -3,13 +3,12 @@ $(document).ready(function() {
 
     $.get("http://localhost:5000/candidates",  function(data)
     {
-        console.log(data)
         data.forEach(function(element) {
             $('#candidateTable tbody').append('<tr><td>'+element.candidateName+'</td><td>'+element.votes+'</td></tr>');
         });
 
         $('#canidateSelect').append(
-        '<option disabled selected value> -- select an option -- </option>');
+        '<option disabled selected value="default"> -- select an option -- </option>');
 
         data.forEach(function(element) {
             $('#canidateSelect').append('<option value="'+element.candidateName+'">'+
@@ -21,20 +20,40 @@ $(document).ready(function() {
 
 function vote()
 {
-    
-    data = { userAddress: $("#userAddress").val(), candidateName: $("#candidate").val() }
-    $.ajax({
-      type: "POST",
-      url: "http://localhost:5000/vote",
-      data: JSON.stringify(data),
-      contentType: "application/json; charset=utf-8",
-      dataType: "json"
-    });
+    var attr = $('#voteButton').attr('disabled');
+
+    if (!(typeof attr !== typeof undefined && attr !== false))
+    {
+        option = $('#canidateSelect').find('option:selected').attr('value')
+        console.log(option)
+        data = { userAddress: $("#userAddress").val(), candidateName: option }
+        $.ajax({
+          type: "POST",
+          url: "http://localhost:5000/vote",
+          data: JSON.stringify(data),
+          contentType: "application/json; charset=utf-8",
+          dataType: "json"
+        });
+    }
+}
+
+function checkVoteEnabled()
+{
+    option = $('#canidateSelect').find('option:selected').attr('value')
+    if( option !== "default" && $("#userAddress").val() !== "")
+    {
+        $('#voteButton').removeAttr('disabled')
+    }
+    else
+    {
+        $('#voteButton').attr('disabled', true)
+    }
+
 
 }
 
+
 function canidateSelect()
 {
-    $('#canidateSelect').append()
-    '<option disabled selected value> -- select an option -- </option>');
+    checkVoteEnabled()
 }
